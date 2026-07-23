f9722daba0e476ecdb06842dc5a23f254e3dcb6b
arthur gabriel <gsthurgato@gmail.com>
2026-07-23 19:43:33 -0300

    AAXSAX\AAASDSSDD

---
+++ players.js
@@ -0,0 +1,30 @@
+window.playersDatabase = [
+
+    {
+        nome: "Kylian Mbappé",
+        clube: "Real Madrid",
+        overall: 91,
+        potencial: 95,
+        posição: "ATA",
+        camisa: 9
+    },
+
+    {
+        nome: "Vinícius Jr.",
+        clube: "Real Madrid",
+        overall: 90,
+        potencial: 92,
+        posição: "PE",
+        camisa: 7
+    },
+
+    {
+        nome: "Jude Bellingham",
+        clube: "Real Madrid",
+        overall: 90,
+        potencial: 94,
+        posição: "MC",
+        camisa: 5
+    }
+
+];
